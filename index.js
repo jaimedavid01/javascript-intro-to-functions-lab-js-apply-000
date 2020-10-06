c3a7776ab6035d8eeaad501529a7c15c6b01211d
stellar-server-0187 <jaimedavid01@yahoo.com>
2020-10-06 05:17:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,7 @@ if (string.toLowerCase() === string){
 if (string.toUpperCase() === string){
   return "YES INDEED!";
 }
-if (string ===)
+if (string === "I love you, Grandma."){
+  return "I love you, too.";
+}
+}
